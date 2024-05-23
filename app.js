a68c8793ac3fd7807ab56777e834e656d4f3bf89
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-05-23 02:18:58 -0700

    Exploring pure react

---
+++ app.js
@@ -0,0 +1,29 @@
+const Person = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.occupation),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { class: "title" }, "React is Rendered"),
+    React.createElement(
+      Person,
+      { name: "Aviwe", occupation: "Software Developer" },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: "Vuyo", occupation: "Software Developer" },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: "Mbuzeli", occupation: "Software Developer" },
+      null
+    ),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
